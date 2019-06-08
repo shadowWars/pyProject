6d5834708ff709ee3b2ba8b484da4027bbc87d70
basanta_gyawali01 <basantagyawali01@gmail.com>
2019-06-08 12:31:17 +0545

    adding input in the code

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 '''-- This is first program through git --'''
 print("this is a new program ")
+name = input("Enter your name : ")
+print("hello ,",name)
+print("Welcome to BG's World")
+print("Thank You for joining BG's World")
+
 

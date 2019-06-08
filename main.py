ee89edb92ac785cc7ede06207de9c4b36fe8591c
basanta_gyawali01 <basantagyawali01@gmail.com>
2019-06-08 12:26:25 +0545

    Fourth commit

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-this is a code for the new line
-this is  only used for other to modify the code in upcomming days 
-only check of the code is done over here
-i am begineer in the github so i am learing over here 
+'''-- This is first program through git --'''
+print("this is a new program ")
+

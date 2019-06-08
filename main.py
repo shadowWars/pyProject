f35661919f87c1648ad236673bdb74a14ab84815
basanta_gyawali01 <basantagyawali01@gmail.com>
2019-06-08 12:05:20 +0545

    Third commit

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
 this is a code for the new line
+this is  only used for other to modify the code in upcomming days 
+only check of the code is done over here
+i am begineer in the github so i am learing over here 
